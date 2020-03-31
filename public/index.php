c7f5c7000753304ddea339dafdacc3a3e2c2c699
werdffelynir <werdffelynir@gmail.com>
2020-03-31 13:25:27 +0300

    public folder outside example

--- public/index.php
+++ public/index.php
@@ -2,7 +2,7 @@
 
 $PUBLIC_ROOT = dirname(__DIR__);
 
-include_once $PUBLIC_ROOT . '/../../vendor/autoload.php';
+include_once $PUBLIC_ROOT . '/vendor/autoload.php';
 
 
 use App\AppCore;
@@ -10,8 +10,8 @@ use App\AppCore;
 
 try {
     new AppCore(
-        include_once $PUBLIC_ROOT . '/config.php',
-        include_once $PUBLIC_ROOT . '/router.php'
+        include_once $PUBLIC_ROOT . '/src/app/config.php',
+        include_once $PUBLIC_ROOT . '/src/app/router.php'
     );
 } catch (Exception $e) {
     /** @noinspection PhpUnhandledExceptionInspection */
--- src/app/Classes/Model.php
+++ src/app/Classes/Model.php
@@ -4,7 +4,7 @@
 namespace App\Classes;
 
 
-class Model
+abstract class Model
 {
 
     /** @type Database */
